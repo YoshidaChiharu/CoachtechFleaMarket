2348e8922adfa26a6ed036f029db8f4c4e5e6bbb
Yoshida Chiharu <gmsp1000.3624@gmail.com>
2024-12-12 15:52:11 +0900

    トップページ用レイアウト作成

---
+++ resources/js/Layouts/TopPageLayout.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, onUpdated, onMounted } from 'vue';
+import { Link } from '@inertiajs/vue3';
+import NavLink from '@/Components/NavLink.vue';
+
+// ログイン状態によってレイアウトを切り替える
+// import Layout from '../Layouts/AuthenticatedLayout.vue';
+import Layout from '../Layouts/GuestLayout.vue';
+
+const selected = ref(1)
+</script>
+
+<template>
+    <Layout>
+    <div class="pt-10 pb-2 px-20 border-b border-gray-600">
+        <div class="max-w-7xl mx-auto">
+            <ul class="flex gap-20">
+                <li @click="selected = 1" :class="{ 'text-red-400' : selected == 1 }">
+                    <Link href="/" class="font-bold">おすすめ</Link>
+                </li>
+                <li @click="selected = 2" :class="{ 'text-red-400' : selected == 2 }">
+                    <Link href="/" class="font-bold">マイリスト</Link>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <slot />
+    </Layout>
+</template>
