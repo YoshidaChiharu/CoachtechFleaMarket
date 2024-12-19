f8ac17c246bf650293cb1b5a6a3142f53e201b35
Yoshida Chiharu <gmsp1000.3624@gmail.com>
2024-12-19 12:27:27 +0900

    商品カードを別コンポーネント（ItemCard.vue）に切り分け

---
+++ resources/js/Components/ItemCard.vue
@@ -0,0 +1,31 @@
+<script setup>
+import LikeIcon from "../Components/LikeIcon.vue";
+import ItemImage from "../Components/ItemImage.vue";
+
+defineProps({
+    item: Array,
+})
+</script>
+
+<template>
+    <div>
+        <div class="relative">
+            <!-- 商品画像 -->
+            <ItemImage :path="item.image_url" />
+            <!-- お気に入りアイコン -->
+            <div
+                class="w-1/5 max-w-8 flex justify-center items-center aspect-square rounded-full bg-gray-200 opacity-50 absolute top-2 right-2"
+            >
+                <LikeIcon class="w-3/5" />
+            </div>
+            <!-- 価格 -->
+            <div
+                class="py-1 pl-2 pr-4 bg-gray-600 opacity-50 inline-block rounded-r-full absolute bottom-0 left-0"
+            >
+                <span class="opacity-100 text-white font-bold">￥{{ item.price }}</span>
+            </div>
+        </div>
+        <!-- 商品名 -->
+        <span>{{ item.name }}</span>
+    </div>
+</template>
