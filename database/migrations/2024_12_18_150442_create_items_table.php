a2585e1ad92cf27338860d98b144e6c7727dd180
Yoshida Chiharu <gmsp1000.3624@gmail.com>
2024-12-28 15:35:48 +0900

    itemsテーブルにbrandカラムを追加

--- database/migrations/2024_12_18_150442_create_items_table.php
+++ database/migrations/2024_12_18_150442_create_items_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('brand')->nullable();
             $table->integer('price');
             $table->string('description');
             $table->string('image_url');
