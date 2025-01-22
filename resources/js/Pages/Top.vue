7465f219165d0359673b8fd320c0b9e1f0abe408
Yoshida Chiharu <gmsp1000.3624@gmail.com>
2025-01-22 11:10:55 +0900

    レスポンシブ対応 - Top.vue/TopPageLayout.vue

--- resources/js/Pages/Top.vue
+++ resources/js/Pages/Top.vue
@@ -12,11 +12,11 @@ defineProps({
 </script>
 
 <template>
-    <div class="max-w-5xl mx-auto py-10">
+    <div class="max-w-5xl mx-auto py-10 px-6">
         <Head title="Top Page" />
         <div class="flex flex-wrap">
             <!-- 商品カード -->
-            <div v-for="item in items.data" :key="item.id" class="w-1/5 p-2">
+            <div v-for="item in items.data" :key="item.id" class="w-1/2 sm:w-1/4 md:w-1/5 p-2">
                 <ItemCard :item="item" />
             </div>
         </div>
