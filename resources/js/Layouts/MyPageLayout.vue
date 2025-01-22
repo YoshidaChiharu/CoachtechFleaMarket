f7a9ac60c5e4c826b30519bc8453a5a7f918442a
Yoshida Chiharu <gmsp1000.3624@gmail.com>
2025-01-22 14:46:34 +0900

    レスポンシブ対応 - MyPage.vue/MyPageLayout.vue

--- resources/js/Layouts/MyPageLayout.vue
+++ resources/js/Layouts/MyPageLayout.vue
@@ -9,22 +9,22 @@ import DefaultLayout from "@/Layouts/DefaultLayout.vue";
 
 <template>
     <DefaultLayout>
-    <div class="pt-10 pb-2 px-20 border-b border-gray-600">
+    <div class="pt-10 pb-2 px-6 md:px-20 border-b border-gray-600">
         <div class="max-w-7xl mx-auto">
-            <div class="flex justify-around items-center mb-10">
-                <div class="flex gap-10 items-center">
-                    <div class="w-32">
+            <div class="flex justify-around items-center mb-10 max-sm:flex-col">
+                <div class="flex gap-10 items-center max-sm:w-full">
+                    <div class="w-24 md:w-32">
                         <UserIcon path="/img/default_user_icon.png" />
                     </div>
                     <span class="font-bold text-2xl">ユーザー名</span>
                 </div>
-                <div>
+                <div class="max-sm:mt-5 max-sm:w-full">
                     <Link href="/mypage/profile">
-                        <SecondaryButton>プロフィールを編集</SecondaryButton>
+                        <SecondaryButton class="w-full">プロフィールを編集</SecondaryButton>
                     </Link>
                 </div>
             </div>
-            <ul class="flex gap-20">
+            <ul class="flex gap-10 md:gap-20">
                 <li :class="{ 'text-red-500' : $page.url === '/mypage' }">
                     <Link href="/mypage" class="font-bold">出品した商品</Link>
                 </li>
