7686db8d5a011f45373a13c21d47f295ea4621ba
Yoshida Chiharu <gmsp1000.3624@gmail.com>
2025-02-04 14:35:09 +0900

    ルーティングにコメント追記＆整形

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 use App\Http\Controllers\TopPageController;
-use App\Http\Controllers\Api\LikeController;
 use App\Http\Controllers\MyPageController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ItemDetailController;
@@ -15,51 +12,94 @@ use App\Http\Controllers\SellController;
 use App\Http\Controllers\Admin\AdminUserController;
 use App\Http\Controllers\Admin\AdminCommentController;
 use App\Http\Controllers\Admin\AdminMailController;
-use App\Http\Controllers\Api\PaymentIntentController;
-use App\Http\Controllers\Api\SoldItemController;
 
-// Route::get('/test', function () { return Inertia::render('Test'); });
+/*
+|--------------------------------------------------------------------------
+| 全ユーザー向けページ
+|--------------------------------------------------------------------------
+*/
+Route::get('/', [TopPageController::class, 'index'])
+    ->name('top');
 
-Route::get('/', [TopPageController::class, 'index'])->name('top');
-Route::get('/item/{item_id}', [ItemDetailController::class, 'show'])->name('item.detail');
+Route::get('/item/{item_id}', [ItemDetailController::class, 'show'])
+    ->name('item.detail');
 
-// 一般ユーザー向けページ
+/*
+|--------------------------------------------------------------------------
+| ログイン済みユーザー向けページ
+|--------------------------------------------------------------------------
+*/
 Route::middleware('verified', 'auth')->group(function () {
-    Route::get('/mylist', [TopPageController::class, 'showMylist'])->name('top.mylist');
-    Route::post('/api/like/{item_id}', [LikeController::class, 'store']);
-    Route::delete('/api/like/{item_id}', [LikeController::class, 'destroy']);
-    Route::get('/mypage', [MyPageController::class, 'index'])->name('mypage');
-    Route::get('/mypage/profile', [ProfileController::class, 'edit'])->name('mypage.profile');
+    // トップページ(マイリスト)
+    Route::get('/mylist', [TopPageController::class, 'showMylist'])
+        ->name('top.mylist');
+
+    // マイページ
+    Route::get('/mypage', [MyPageController::class, 'index'])
+        ->name('mypage');
+
+    Route::get('/mypage/profile', [ProfileController::class, 'edit'])
+        ->name('mypage.profile');
+
     Route::post('/mypage/profile', [ProfileController::class, 'update']);
-    Route::get('/item/comment/{item_id}', [CommentController::class, 'index'])->name('item.comment');
+
+    // 商品詳細＞コメントページ
+    Route::get('/item/comment/{item_id}', [CommentController::class, 'index'])
+        ->name('item.comment');
+
     Route::post('/item/comment/{item_id}', [CommentController::class, 'store']);
 
-    Route::get('/purchase/{item_id}', [PurchaseController::class, 'create'])->name('purchase');
-    Route::post('/api/purchase/{item_id}', [PaymentIntentController::class, 'createPaymentIntent']);
-    Route::get('/api/purchase/sold_item/{item_id}', [SoldItemController::class, 'confirmStock']);
-    Route::post('/api/purchase/sold_item/{item_id}', [SoldItemController::class, 'store']);
-    Route::delete('/api/purchase/sold_item/{sold_item_id}', [SoldItemController::class, 'destroy']);
-    Route::get('/purchase/complete/{item_id}', [PurchaseController::class, 'confirmPaymentIntentStatus'])->name('purchase.complete');
+    // 購入ページ
+    Route::get('/purchase/{item_id}', [PurchaseController::class, 'create'])
+        ->name('purchase');
 
-    Route::get('/purchase/address/register', [ShipAddressController::class, 'create'])->name('purchase.address.register');
+    Route::get('/purchase/complete/{item_id}', [PurchaseController::class, 'confirmPaymentIntentStatus'])
+        ->name('purchase.complete');
+
+    // 購入ページ＞配送先変更
+    Route::get('/purchase/address/register', [ShipAddressController::class, 'create'])
+        ->name('purchase.address.register');
+
     Route::post('/purchase/address/register', [ShipAddressController::class, 'store']);
 
-    Route::get('/purchase/address/edit/{address_id}', [ShipAddressController::class, 'edit'])->name('purchase.address.edit');
+    Route::get('/purchase/address/edit/{address_id}', [ShipAddressController::class, 'edit'])
+        ->name('purchase.address.edit');
+
     Route::post('/purchase/address/edit/{address_id}', [ShipAddressController::class, 'update']);
+
     Route::delete('/purchase/address/edit/{address_id}', [ShipAddressController::class, 'destroy']);
 
-    Route::get('/sell', [SellController::class, 'create'])->name('sell');
+    // 出品ページ
+    Route::get('/sell', [SellController::class, 'create'])
+        ->name('sell');
+
     Route::post('/sell', [SellController::class, 'store']);
 });
 
-// 管理者専用ページ
+/*
+|--------------------------------------------------------------------------
+| 管理者専用ページ
+|--------------------------------------------------------------------------
+*/
 Route::middleware('auth', 'admin')->group(function () {
-    Route::get('/admin/user', [AdminUserController::class, 'index'])->name('admin.user');
+    // ユーザー管理ページ
+    Route::get('/admin/user', [AdminUserController::class, 'index'])
+        ->name('admin.user');
+
     Route::post('/admin/user', [AdminUserController::class, 'destroy']);
-    Route::get('/admin/comment', [AdminCommentController::class, 'index'])->name('admin.comment');
+
+    // コメント管理ページ
+    Route::get('/admin/comment', [AdminCommentController::class, 'index'])
+        ->name('admin.comment');
+
     Route::post('/admin/comment', [AdminCommentController::class, 'destroy']);
-    Route::get('/admin/mail', [AdminMailController::class, 'create'])->name('admin.mail');
+
+    // メール送信ページ
+    Route::get('/admin/mail', [AdminMailController::class, 'create'])
+        ->name('admin.mail');
+
     Route::post('/admin/mail', [AdminMailController::class, 'sendMail']);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__.'/auth.php'; // 認証関連ルーティング
+require __DIR__.'/api.php';  // API用ルーティング
