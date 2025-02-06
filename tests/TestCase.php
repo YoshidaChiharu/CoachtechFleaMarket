7321ef052fcc902db303922224dbafe63aa54cef
Yoshida Chiharu <gmsp1000.3624@gmail.com>
2025-02-06 19:41:54 +0900

    テスト開始時にダミーデータのシーディングを追加、及びこれに伴う影響箇所を修正

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,13 @@
 
 namespace Tests;
 
+use Database\Seeders\DatabaseSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase;
+
+    protected string $seeder = DatabaseSeeder::class;
 }
